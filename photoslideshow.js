4efb0bfea5d4916014674878146c6839c6ce2a69
Michael Moua <mikelmoua@gmail.com>
2022-03-24 22:07:08 -0400

    Fixed lines of code

--- photoslideshow.js
+++ photoslideshow.js
@@ -1,4 +1,26 @@
-const photoSlideShow {
-    photoList = ['corgipic1', 'corgipic2', 'corgipic3', 'corgipic4'];
-    const currentPhotoIndex = photoList => photoList[0];
+const photoSlideShow = {
+    photoList: ['corgipic1', 'corgipic2', 'corgipic3', 'corgipic4'],
+    currentPhotoIndex: 0,
+    nextPhoto: function() {
+        if (this.currentPhotoIndex < this.photoList.length -1) {
+                this.currentPhotoIndex++;
+                console.log(this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('End of slideshow');
+        }
+    },
+    prevPhoto: function() {
+        if (this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            console.log(this.photoList[this.currentPhotoIndex]);
+        } else {
+            console.log('Beginning of slideshow');
+        }
+    },
+    getCurrentPhoto() {
+        console.log(this.photoList[this.currentPhotoIndex]);
+    }
 }
+photoSlideShow.nextPhoto();
+photoSlideShow.prevPhoto();
+photoSlideShow.getCurrentPhoto();
